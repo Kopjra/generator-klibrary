a07cfaf03e03942ea0e7b6d873dcdf942b4816c5
Luis Perez Sanchez <luis.perezsanchez@kopjra.com>
2019-01-09 16:48:32 +0100

    finally fixed npm install, and added folder creation question

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,7 +2,7 @@
 const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
-const glob = require("glob-promise");
+const glob = require("glob");
 
 module.exports = class extends Generator {
     async prompting() {
@@ -15,34 +15,45 @@ module.exports = class extends Generator {
             {
                 type: "input",
                 name: "libName",
-                message: "Library Name",
-                default: this.appname
+                message: "Library Name (no spaces)"
+            },
+            {
+                type: "confirm",
+                name: "createFolder",
+                message: "Do you want to create a folder?"
             }
         ]);
     }
 
-    async writing() {
-        const paths = (await glob(this.templatePath("**/!(*RENAMED)"), {
-            dot: true
-        })).map(path => path.substring(this.templatePath().length + 1));
+    writing() {
+        if (this.answers.createFolder) {
+            this.destinationRoot(
+                `${this.destinationRoot()}/${this.answers.libName}`
+            );
+        }
+        const paths = glob
+            .sync(this.templatePath("**/!(*RENAMED)"), {
+                dot: true
+            })
+            .map(path => path.substring(this.templatePath().length + 1));
 
         for (const path of paths) {
-            await this.fs.copyTpl(
+            this.fs.copyTpl(
                 this.templatePath(path),
                 this.destinationPath(path),
                 this.answers
             );
         }
 
-        await this.fs.copyTpl(
+        this.fs.copyTpl(
             this.templatePath(".npmignoreRENAMED"),
             this.destinationPath(".npmignore"),
             this.answers
         );
     }
 
-    async installDeps() {
-        await this.npmInstall(
+    install() {
+        this.npmInstall(
             [
                 "@types/chai",
                 "@types/chai-as-promised",
@@ -67,7 +78,17 @@ module.exports = class extends Generator {
                 "typescript"
             ],
             { "save-dev": true }
+        ).then();
+        this.npmInstall(["joi", "nconf"], { save: true }).then();
+    }
+
+    end() {
+        this.log(
+            yosay(
+                `Everything is ready. Search for ${chalk.yellow(
+                    "TODO"
+                )} markers in the code and follow the instruction in the comments.`
+            )
         );
-        await this.npmInstall(["joi", "nconf"], { save: true });
     }
 };
